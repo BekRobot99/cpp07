f5113a643b330269a3a24cf1d55c3379e63ff529
abekri <abekri@student.42heilbronn.de>
2024-12-27 17:45:45 +0100

    set project as finished

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -10,12 +10,4 @@ void iter(T* array, std::size_t length, Func func) {
     }
 }
 
-template <typename T, typename Func>
-void iter(const T* array, std::size_t length, Func func) {
-    for (std::size_t i = 0; i < length; ++i) {
-        func(array[i]);
-    }
-}
-
 #endif 
-
